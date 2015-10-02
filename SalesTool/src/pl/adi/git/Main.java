f9955a9fffdcc926e68b08c6f9c0a6e1ab4dfe37
Adrian <adrspo95@o2.pl>
2015-10-02 12:34:57 +0200

    First implementation of Main class

--- SalesTool/src/pl/adi/git/Main.java
+++ SalesTool/src/pl/adi/git/Main.java
@@ -3,8 +3,17 @@ package pl.adi.git;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		SalesData data = new SalesData();
+		
+		displayGreetings();
+		data.display();
+	}
 
+	private static void displayGreetings() {
+		
+		System.out.println("HELLO HAPPY SALES PEOPLE");
+		System.out.println("THIS APP SHOWS SALES DATA");
 	}
 
 }
